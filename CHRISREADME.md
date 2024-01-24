8f9f0f48d904f4b7de741d836a27d2f9681d7c00
Chris Lopez <chris.bradyl26@gmail.com>
2024-01-23 18:35:03 -0600

    Strikethrough and Vercel.json added

--- CHRISREADME.md
+++ CHRISREADME.md
@@ -3,3 +3,7 @@ Cd into app
 To start env use source env/bin/activate
 
 Then use python3 app.py
+
+
+
+You can click on an entry and strike through it when you are done with it.
--- app/static/todoList.js
+++ app/static/todoList.js
@@ -1,16 +1,20 @@
 document.getElementById("TodoSubmit").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevents the default form submission behavior
+    event.preventDefault();
     addTodo();
 }, false);
 
-document.getElementById("TodoList").addEventListener("click", addTodo, false);
-
+document.getElementById("TodoList").addEventListener("click", strikeOutItem, false);
 
 function addTodo() {
     let todoNode = document.createElement("li");
     let itemToAdd = document.getElementById("TodoInput").value;
-    let newNode = document.createTextNode(itemToAdd); // Corrected typo here
+    let newNode = document.createTextNode(itemToAdd);
     todoNode.appendChild(newNode);
     document.getElementById("TodoList").appendChild(todoNode);
     document.getElementById("TodoInput").value = "";
+}
+
+function strikeOutItem(event) {
+    let clickedItem = event.target;
+    clickedItem.classList.toggle('strike');
 }
